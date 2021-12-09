719171f04b44d6794fab7ba66b5fa3be07da0271
Amadeus <38767929+Mozart409@users.noreply.github.com>
2021-12-09 13:41:44 +0100

    build: :building_construction: added noninteractive

--- setup-debian.sh
+++ setup-debian.sh
@@ -2,7 +2,7 @@
 
 if [ `whoami` != root ]; then
     echo -e "Please run this script as root or using \e[31msudo"
-    echo  
+    echo
     exit
 fi
 
@@ -10,24 +10,24 @@ do_space(){
     echo -e "\n \n"
 }
 
-
+export DEBIAN_FRONTEND=noninteractive
 do_space
-echo "Updating packages"
+echo "Updating sources"
 apt update -y
 do_space
 echo "Installing new packages"
-apt install -y fonts-firacode git zsh curl wget 
+apt-get -q -y install fonts-firacode git zsh curl wget
 
 do_space
 echo "installing Github Desktop"
 wget -qO - https://packagecloud.io/shiftkey/desktop/gpgkey | tee /etc/apt/trusted.gpg.d/shiftkey-desktop.asc > /dev/null
 sh -c 'echo "deb [arch=amd64] https://packagecloud.io/shiftkey/desktop/any/ any main" > /etc/apt/sources.list.d/packagecloud-shiftky-desktop.list'
 apt-get update
-apt install github-desktop -y
+apt-get -q -y github-desktop
 
 do_space
 echo "installing antigen and download .zshrc"
 curl -L https://raw.githubusercontent.com/Mozart409/.dotfiles/master/zsh/.zshrc > ~/.zshrc
-curl -L git.io/antigen > ~/antigen.zsh
 zsh
 chsh -s $(which zsh)
+curl -L git.io/antigen > ~/antigen.zsh
